0b4faf5bf70f76ee369c5b2374c6d6f558724b46
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2024-11-22 13:22:45 -0300

    agregue menu y menurol para cliente

--- Vista/Menu/Action/actionRegistrar.php
+++ Vista/Menu/Action/actionRegistrar.php
@@ -39,6 +39,67 @@ if (isset($datos)) {
 
         $res3 = $objAbmUsuarioRol->alta($nuevaColObjRol); // se agregan los datos del objeto usuarioRol en la BD
 
+        /* ========== parte de menu ===========*/
+        $objAbmMenuHogar = new AbmMenu();
+        $objAbmMenuProductos = new AbmMenu();
+        $objAbmMenuMiPerfil = new AbmMenu();
+        $objAbmMenuCarrito = new AbmMenu();
+        $objAbmMenuCerrarSesion = new AbmMenu();
+
+        $arrayHogar = ["idmenu" => null, "menombre" => "Hogar", "medescripcion" => "menu.php", "idpadre" => null, "medeshabilitado" => "0000-00-00 00:00:00"];
+        $arrayProductos = ["idmenu" => null, "menombre" => "Productos", "medescripcion" => "productos.php", "idpadre" => null, "medeshabilitado" => "0000-00-00 00:00:00"];
+        $arrayMiPerfil = ["idmenu" => null, "menombre" => "Mi Perfil", "medescripcion" => "perfilUser.php", "idpadre" => null, "medeshabilitado" => "0000-00-00 00:00:00"];
+        $arrayCarrito = ["idmenu" => null, "menombre" => "Carrito", "medescripcion" => "carrito.php", "idpadre" => null, "medeshabilitado" => "0000-00-00 00:00:00"];
+        $arrayMenuCerrarSesion = ["idmenu" => null, "menombre" => "Cerrar Sesion", "medescripcion" => "Action/actionVerificarLogin.php?accion=cerrar", "idpadre" => null, "medeshabilitado" => "0000-00-00 00:00:00"];
+
+        $respuestaHogar = $objAbmMenuHogar->alta($arrayHogar);
+        $respuestaProductos = $objAbmMenuProductos->alta($arrayProductos);
+        $respuestaMiPerfil = $objAbmMenuMiPerfil->alta($arrayMiPerfil);
+        $respuestaCarrito = $objAbmMenuCarrito->alta($arrayCarrito);
+        $respuestaCerrarSesion = $objAbmMenuCerrarSesion->alta($arrayMenuCerrarSesion);
+
+
+
+        /* ========== parte de menurol ===========*/
+        $objAbmMenuRolHogar = new AbmMenuRol();
+        $colMenuHogar = $objAbmMenuHogar->buscar($arrayHogar);
+        $contador1 = count($colMenuHogar) - 1;
+        $colMenuHogar[$contador1]; // obtiene el ultimo objeto del menu 
+        $respMeRolHogar = $objAbmMenuRolHogar->alta(["idmenu" => $colMenuHogar[$contador1], "idrol" => $objRol[0]]);
+
+        //echo "<script>console.log(" . json_encode($contador1) . ");</script>";
+        //verEstructuraJson($colMenuHogar[$contador1]);
+        //verEstructuraJson($objRol[0]);
+
+
+        $objAbmMenuRolProdcutos = new AbmMenuRol();
+        $colMenuProductos = $objAbmMenuProductos->buscar($arrayProductos);
+        $contador2 = count($colMenuProductos) - 1;
+        $colMenuProductos[$contador2]; // obtiene el ultimo objeto del menu 
+        $respMeRolProducto = $objAbmMenuRolProdcutos->alta(["idmenu" => $colMenuProductos[$contador2], "idrol" => $objRol[0]]);
+
+
+        $objAbmMenuRolMiPerfil = new AbmMenuRol();
+        $colMenuMiPerfil = $objAbmMenuMiPerfil->buscar($arrayMiPerfil);
+        $contador3 = count($colMenuMiPerfil) - 1;
+        $colMenuMiPerfil[$contador3]; // obtiene el ultimo objeto del menu
+        $respMeRolMiperfil = $objAbmMenuRolMiPerfil->alta(["idmenu" => $colMenuMiPerfil[$contador3], "idrol" => $objRol[0]]);
+
+
+        $objAbmMenuRolCarrito = new AbmMenuRol();
+        $colMenuCarrito = $objAbmMenuCarrito->buscar($arrayCarrito);
+        $contador4 = count($colMenuCarrito) - 1;
+        $colMenuCarrito[$contador4]; // obtiene el ultimo objeto del menu 
+        $respMeRolCarrito = $objAbmMenuRolCarrito->alta(["idmenu" => $colMenuCarrito[$contador4], "idrol" => $objRol[0]]);
+
+
+        $objAbmMenuRolCerrarSesion = new AbmMenuRol();
+        $colMenuCerrarSesion = $objAbmMenuCerrarSesion->buscar($arrayMenuCerrarSesion);
+        $contador5 = count($colMenuCerrarSesion) - 1;
+        $colMenuCerrarSesion[$contador5]; // obtiene el ultimo objeto del menu 
+        $respMeRolCerrarSesion = $objAbmMenuRolCerrarSesion->alta(["idmenu" => $colMenuCerrarSesion[$contador5], "idrol" => $objRol[0]]);
+
+
         if ($res1 && $res2 && $res3) { // dar mensaje para el h1 de titulo
             $mensajeRegistro = [
                 'respuesta' => true,
