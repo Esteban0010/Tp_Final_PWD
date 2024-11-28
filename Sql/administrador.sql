61e3ece6586824a19ce2675a964c32a0e1693cfa
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2024-11-28 07:30:40 -0300

    Ahora si funciona el sql del administrador

--- Sql/administrador.sql
+++ Sql/administrador.sql
@@ -1,7 +1,7 @@
 /* usuario */
 INSERT INTO `usuario` (`idusuario`, `usnombre`, `uspass`, `usmail`, `usdeshabilitado`) 
 VALUES 
-(1, 'pedro', 'c4ca4238a0b923820dcc509a6f75849b', 'pedro@gmail.com', NULL);
+(1, 'pedro', 'c4ca4238a0b923820dcc509a6f75849b', 'pedro@gmail.com', '0000-00-00 00:00:00');
 /* uspass = 1 */
 
 /* rol */
@@ -10,18 +10,20 @@ VALUES
 (1, 'administrador');
 
 /* usuariorol */
-INSERT INTO `usuariorol`(`idusuario`, `idrol`) VALUES (1,1);
+INSERT INTO `usuariorol` (`idusuario`, `idrol`) 
+VALUES 
+(1, 1);
 
 /* menu */
 INSERT INTO `menu` (`idmenu`, `menombre`, `medescripcion`, `idpadre`, `medeshabilitado`) 
 VALUES 
-(1, 'Hogar', 'menu.php', NULL, NULL),
-(2, 'Productos', 'productos.php', NULL, NULL),
-(3, 'Mi Perfil', 'perfilUser.php', NULL, NULL),
-(4, 'Carrito', 'carrito.php', NULL, NULL),
-(5, 'Cerrar Sesion', 'Action/actionVerificarLogin.php?accion=cerrar', NULL, NULL),
-(6, 'Gestionar Compra', 'gestionarCompra3.php', NULL, NULL),
-(7, 'Administrador', 'configurarAdmin.php', NULL, NULL);
+(1, 'Hogar', 'menu.php', NULL, '0000-00-00 00:00:00'),
+(2, 'Productos', 'productos.php', NULL, '0000-00-00 00:00:00'),
+(3, 'Mi Perfil', 'perfilUser.php', NULL, '0000-00-00 00:00:00'),
+(4, 'Carrito', 'carrito.php', NULL, '0000-00-00 00:00:00'),
+(5, 'Cerrar Sesion', 'Action/actionVerificarLogin.php?accion=cerrar', NULL, '0000-00-00 00:00:00'),
+(6, 'Gestionar Compra', 'gestionarCompra3.php', NULL, '0000-00-00 00:00:00'),
+(7, 'Administrador', 'configurarAdmin.php', NULL, '0000-00-00 00:00:00');
 
 /* menurol */
 INSERT INTO `menurol` (`idmenu`, `idrol`) 
